ecd59b8adb8c78b79d698bde704fca5198423932
musademirezer01-dev <musademirezer01@gmail.com>
2026-04-03 22:16:57 +0300

    Add main index file for WooCommerce and posts

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Main index file
+ */
+get_template_part('head'); // head.php'yi ekler
+
+// WooCommerce aktifse ürünleri listele
+if ( class_exists('WooCommerce') ) :
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => 16,
+        'post_status' => 'publish'
+    );
+    $products = new WP_Query($args);
+
+    if ($products->have_posts()) : ?>
+      <div class="grid-container">
+      <?php while ($products->have_posts()) : $products->the_post();
+        global $product;
+        ?>
+        <a class="product-card" href="<?php the_permalink(); ?>">
+            <?php if (has_post_thumbnail()) {
+              the_post_thumbnail('large');
+            } else { ?>
+              <img src="<?php echo wc_placeholder_img_src(); ?>" alt="">
+            <?php } ?>
+            <div class="product-info">
+              <?php the_title(); ?><br>
+              <?php echo $product->get_price_html(); ?>
+            </div>
+        </a>
+      <?php endwhile; ?>
+      </div>
+      <?php wp_reset_postdata();
+    else : ?>
+      <p style="text-align:center">No products found.</p>
+    <?php endif;
+else: // WooCommerce yoksa klasik yazıları göster
+  if (have_posts()) : ?>
+    <div class="grid-container">
+    <?php while (have_posts()) : the_post(); ?>
+      <a class="product-card" href="<?php the_permalink(); ?>">
+        <?php if (has_post_thumbnail()) {
+          the_post_thumbnail('large');
+        } else { ?>
+          <img src="<?php echo get_template_directory_uri(); ?>/placeholder.png" alt="">
+        <?php } ?>
+        <div class="product-info">
+          <?php the_title(); ?><br>
+        </div>
+      </a>
+    <?php endwhile; ?>
+    </div>
+    <?php else : ?>
+      <p style="text-align:center">No content found.</p>
+    <?php endif;
+endif;
+
+get_template_part('footer'); // footer.php'yi ekler
